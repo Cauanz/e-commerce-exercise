6b3b7b2155c25f631fc4879e6dec6f84002d9dd2
Cauanz <61758981+Cauanz@users.noreply.github.com>
2024-01-24 11:41:41 -0300

    fazendo header funcionar conforme muda paginas e fazendo pagina addprodutos

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,29 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Header from './components/Header'
+import AddProduto from './pages/AddProduto';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  //TODO - Fazer as funções no App e descer elas pelos componentes
+
+  const [produtos, setProdutos] = useState([]);
+
+  const [nomeProduto, setNomeProduto] = useState('');
+  const [descricaoProduto, setDescricaoProduto] = useState('');
+
+  function handleProduto(e) {
+    const produto = {
+      id: produtos.length,
+      nome: nomeProduto,
+      descricao: descricaoProduto
+    }
+
+    setProdutos([...produtos, produto])
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header />
     </>
   )
 }
